f97d0d0f0c3f9bb4d1700d24e7159ebea4b7419a
kart9071 <karthikakala13@gamail.com>
2022-12-21 21:52:05 +0530

    special day

--- backend_user_interface/Home.php
+++ backend_user_interface/Home.php
@@ -6,15 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/style1.css" type="text/css">
 </head>
 
 <body>
-    Youre a backend user then
-    <form method="post">
-        <input type="text" name="username">
-        <input type="text" name="password">
-        <input type="submit" name="submit">
+    <div class="wrapper">
+    <form class="p-3 mt-3" method="post" >
+            <div class="form-field d-flex align-items-center">
+                <span class="far fa-user"></span>
+                <input type="text" name="username" id="password" placeholder="Enter your name">
+            </div>
+            <div class="form-field d-flex align-items-center">
+                <span class="fas fa-key"></span>
+                <input type="password" name="password" id="password" placeholder="Enter your password">
+            </div>
+            <button class="btn mt-3" type="submit" name="submit">Register</button>
+
     </form>
+    </div>
+
     <?php
     include_once "../Users_interface/mysql.php";
     if (isset($_POST['submit'])) {
@@ -28,8 +38,7 @@
                 window.location.assign("http://localhost/Voting_management_system/backend_user_interface/Entry.php");
             </script>
     <?php
-        }
-        else{
+        } else {
             echo "your password is wrong...";
         }
     }
